98037b412121fe0f1973690d4085dd7df84330fd
marioschindler <dj.simez3@googlemail.com>
2023-06-12 13:09:09 +0200

    Seiten angelegt

---
+++ src/main/kotlin/GlobalThinks.kt
@@ -0,0 +1,19 @@
+
+
+//TODO Globale Listen
+//Fahrerliste Auto-Motorsport
+
+var driverCar = mutableListOf<String>("Mario Schindler", "Enes Kasap", "Eduard Lukanskyy",
+                                      "Peter Pan", "Bratwurst Bernhard", "Hubertus Humbuck",
+                                      "Barnabas Basilius", "Steckzapfen Steffan", "Holz Harald",
+                                      "Thomas Müller", "Mett Machmut", "Wurstfach Werner")
+
+var jockeys = mutableListOf<String>("Oscar Schindler", "Leonardo DaVinci", "Arnold Schwarzenegger",
+                                    "Thomas Mann", "Ernst Thälmann", "Christopher Columbus",
+                                    "Alice Weidel", "Shawn Michaels", "Archibald Blake")
+
+var driverBike = mutableListOf<String>("Märchen Mohamet", "Porno Paul", "Dängel Dethlef",
+                                       "Gustaf Ganz", "Mila Superstar", "Sailor Moon",
+                                       "Dagobert Duck", "Tick Trick und Track", "Donald Duck",
+                                        "Die Panzerknacker", "Daisy Duck")
+
