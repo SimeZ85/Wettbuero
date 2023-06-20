fd85bd7b0c47b19731e3c0de1000a9fbfa2280e0
marioschindler <dj.simez3@googlemail.com>
2023-06-20 22:20:42 +0200

    Seiten angelegt, Menü angelegt, Simulation angelegt, DTM angelegt, Ein und Auszahlen angelegt, Kundenportal fertig, Pferderennen hinzugefügt, Motorradrennen hinzugefügt, neues Spiel Roulette

--- src/main/kotlin/08Roulette.kt
+++ src/main/kotlin/08Roulette.kt
@@ -23,16 +23,17 @@ class Roulette {
             Übersicht:
             ___________________________________________
             
-            1: Eine Farbe ------- Schwarz oder Rot (--x2--)
-            2: Eine Zahl -------- Ein Zahlenfeld von 0 bis 36 (--x35--)
-            3: Zwei Zahlen ------ Zwei Zahlen, die neben oder übereinander sind (--x17--)
-            4: Drei Zahlen ------ Drei Zahlen in einer Reihe (--x11--)
-            5: Vier Zahlen ------ Vier Zahlen, die sich eine Ecke teilen (--x8--)
-            6: 1 -18 ------------ Alle Zahlen von 1 bis 18 (--x2--)
-            7: 19 - 36 ---------- Alle Zahlen von 19 bis 36 (--x2--)
-            8: Gerade ----------- Alle geraden Zahlen (--x2--)
-            9: Ungerade --------- Alle ungeraden Zahlen (--x2--)
-            10: Niedriger/Höher - Höher oder niedrigere Zahlen (--x2--)
+            1:  Eine Farbe ------- Schwarz oder Rot (--x2--)
+            2:  Eine Zahl -------- Ein Zahlenfeld von 0 bis 36 (--x35--)
+            3:  Zwei Zahlen ------ Zwei Zahlen, die neben oder übereinander sind (--x17--)
+            4:  Drei Zahlen ------ Drei Zahlen in einer Reihe (--x11--)
+            5:  Vier Zahlen ------ Vier Zahlen, die sich eine Ecke teilen (--x8--)
+            6:  1 -18 ------------ Alle Zahlen von 1 bis 18 (--x2--)
+            7:  19 - 36 ---------- Alle Zahlen von 19 bis 36 (--x2--)
+            8:  Gerade ----------- Alle geraden Zahlen (--x2--)
+            9:  Ungerade --------- Alle ungeraden Zahlen (--x2--)
+            10: Niedriger/Höher -- Höher oder niedrigere Zahlen (--x2--)
+            11: Zurück zum Menü
             
             
                 3   6   9  12  15  18  21  24  27  30  33  36
@@ -70,6 +71,7 @@ class Roulette {
                 time05()
                 println("Dreht")
                 time05()
+                println("Und die Zahl ist :$rouletteRadZahl und die Farbe ist: ${roulette[rouletteRadZahl]}")
                 if((schwarzRot==1)&&(roulette[rouletteRadZahl]!! == "Rot")){
                     var winAusRot = einsatzAusRot * 2
                     kontostandAccount = kontostandAccount+winAusRot
@@ -78,6 +80,10 @@ class Roulette {
                     println("Dein Gewinn beträgt: $winAusRot€")
                     time05()
                     println("Dein aktuelles Guthaben beträgt: $kontostandAccount€")
+                    time05()
+                    println("Weiter mit Enter")
+                    readln()
+                    spiel()
                 } else if ((schwarzRot==2)&&(roulette[rouletteRadZahl]!! == "Schwarz")) {
                     var winAusSchwarz = einsatzAusRot * 2
                     kontostandAccount = kontostandAccount + winAusSchwarz
@@ -86,15 +92,64 @@ class Roulette {
                     println("Dein Gewinn beträgt: $winAusSchwarz€")
                     time05()
                     println("Dein aktuelles Guthaben beträgt: $kontostandAccount€")
+                    time05()
+                    println("Weiter mit Enter")
+                    readln()
+                    spiel()
                 } else {
                     kontostandAccount = kontostandAccount - einsatzAusRot
                     println("Du hast leider verloren. Vielleicht beim nächsten mal.")
                     time05()
                     println("Dein aktueller Kontostand beträgt: $kontostandAccount")
+                    time05()
+                    println("Weiter mit Enter")
+                    readln()
+                    spiel()
                 }
             }
             vorauswahl==2 -> {
                 "EINE ZAHL"
+                println("Wähle einen Einsatz:")
+                var zahleEinsatz = readln().toDouble()
+                println("Gut, wähle jetzt die Zahl, auf die du setzen möchtest:")
+                var aufZahl = readln().toInt()
+                println("Starten mit Enter")
+                readln()
+                println("Dreht")
+                time05()
+                println("Dreht")
+                time05()
+                println("Dreht")
+                time05()
+                println("Dreht")
+                time05()
+                println("Dreht")
+                time05()
+                println("Dreht")
+                time05()
+                println("Und die Zahl ist :$rouletteRadZahl und die Farbe ist: ${roulette[rouletteRadZahl]}")
+                var gewinnEineZahl = zahleEinsatz * 35
+                if(rouletteRadZahl==aufZahl){
+                    kontostandAccount = kontostandAccount + gewinnEineZahl
+                    println("Herzlichen Glückwunsch du hast gewonnen.")
+                    time05()
+                    println("Du hast $gewinnEineZahl€ gewonnen.")
+                    time05()
+                    println("Dein neues Accountguthaben beträgt: $kontostandAccount€.")
+                    time05()
+                    println("Weiter mit Enter")
+                    readln()
+                    spiel()
+                } else {
+                    kontostandAccount = kontostandAccount - zahleEinsatz
+                    println("Du hast leider verloren. Versuch es erneut.")
+                    time05()
+                    println("Dein aktuelles Accountguthaben beträgt: $kontostandAccount€.")
+                    time05()
+                    println("Weiter mit Enter")
+                    readln()
+                    spiel()
+                }
             }
             vorauswahl==3 -> {
                 "ZWEI ZAHLEN"
@@ -119,6 +174,13 @@ class Roulette {
             }
             vorauswahl==10 -> {
                 "NIEDRIGER HÖHER"
+            }
+            vorauswahl==11 -> {
+                rouletteMenue()
+            }
+            vorauswahl>=12 -> {
+                println("Fehlerhafte Eingabe")
+                spiel()
             }
         }
     }
